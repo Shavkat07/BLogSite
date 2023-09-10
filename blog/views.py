2068ce1d9769d066e59c4127527d62f55afea583
Shavkat <shavkatkurbanov065@gmail.com>
2023-09-10 18:33:31 +0500

    Система комментариев сделано

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,65 @@
-from django.http import Http404
-from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+from django.core.paginator import Paginator, EmptyPage, \
+    PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm, CommentForm
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
-from .models import Post
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    comment = None
+    # Комментарий был отправлен
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        # Создать объект класса Comment, не сохраняя его в базе данных
+        comment = form.save(commit=False)  # commit=False - не сохранять в БД
+        # print(comment)
+        # Назначить текущий пост комментарию
+        comment.post = post
+        # Сохранить комментарий в базе данных
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                  {'post': post,
+                   'form': form,
+                   'comment': comment})
+
+
+def post_share(request, post_id):
+    # Извлечь пост по идентификатору id
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        # Форма была передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject,
+                      message,
+                      'storeserver065@yandex.com',
+                      [cd['to']])
+            sent = True
+            # ... отправить электронное письмо
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
 
 
 class PostListView(ListView):
@@ -16,26 +72,26 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    # Постраничная разбивка с 3 постами на страницу
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # Если page_number не целое число, то
-        # выдать первую страницу
-        posts = paginator.page(1)
-
-    except EmptyPage:
-        # Если page_number находится вне диапазона, то
-        # выдать последнюю страницу
-        posts = paginator.page(paginator.num_pages)
-
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': posts})
+# def post_list(request):
+#     post_list = Post.published.all()
+#     # Постраничная разбивка с 3 постами на страницу
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         # Если page_number не целое число, то
+#         # выдать первую страницу
+#         posts = paginator.page(1)
+#
+#     except EmptyPage:
+#         # Если page_number находится вне диапазона, то
+#         # выдать последнюю страницу
+#         posts = paginator.page(paginator.num_pages)
+#
+#     return render(request,
+#                   'blog/post/list.html',
+#                   {'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
@@ -45,5 +101,11 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    # Список активных комментариев к этому посту
+    comments = post.comments.filter(active=True)
+    # Форма для комментирования пользователями
+    form = CommentForm()
 
-    return render(request, 'blog/post/detail.html', {'post': post})
+    return render(request, 'blog/post/detail.html', {'post': post,
+                                                     'comments': comments,
+                                                     'form': form})
--- mysite/settings.py
+++ mysite/settings.py
@@ -107,6 +107,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+EMAIL_HOST = "smtp.yandex.com"
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "storeserver065@yandex.com"
+EMAIL_HOST_PASSWORD = "ghpcbtjxqiyctqmz"
+EMAIL_USE_SSL = True
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
